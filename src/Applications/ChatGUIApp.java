8b7a030cc114bb846ec6b938f0cdb63f878c14da
Jordan Barnes <rjordanbarnes@gmail.com>
2020-03-19 13:20:12 -0700

    Created general layout of GUI app

---
+++ src/Applications/ChatGUIApp.java
@@ -0,0 +1,23 @@
+package Applications;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ChatGUIApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ChatGUIApp.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
